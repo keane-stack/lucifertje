22838a379383aa147fa7f212b4be6061edd6c80e
bartrl <74553307+Bart-tabak@users.noreply.github.com>
2021-06-09 11:44:49 +0000

    Spelregels toegevoegd

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print('Naam invullen:')
 x = input()
 print('Gegroet ' + x + ', Welkom bij ons luciferspel!')
+
+print('Het spel gaat als volgt: De computer kiest een willekeurig aantal lucifers tussen de 20 en 25. Dat aantal wordt door de computer als getal aan je getoond. Als speler heb je als eerste de keuze om 1, 2 of 3 lucifers weg te nemen. Daarna is het de beurt aan de computer. Ook de computer moet 1, 2 of 3 lucifers wegnemen, waarna de speler weer aan de beurt is, tot er geen lucifers meer over zijn. Degene die als laatste een lucifer MOET wegnemen, heeft verloren.'
+ )
+
