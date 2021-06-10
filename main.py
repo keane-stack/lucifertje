e16b9c2355f4c8c20882a4ea3c48213b073ea5a5
keanestack <74552264+keane-stack@users.noreply.github.com>
2021-06-10 19:35:35 +0000

    kan aantal stokjes pakken

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import random
+
+num1 = random.randint(20, 25)
+sticks = (num1)
+
 print('Naam invullen:')
 x = input()
 print('Gegroet ' + x + ', Welkom bij ons luciferspel!')
 print('')
 print('Het spel gaat als volgt: De computer kiest een willekeurig aantal lucifers tussen de 20 en 25. Dat aantal wordt door de computer als getal aan je getoond. Als speler heb je als eerste de keuze om 1, 2 of 3 lucifers weg te nemen. Daarna is het de beurt aan de computer. Ook de computer moet 1, 2 of 3 lucifers wegnemen, waarna de speler weer aan de beurt is, tot er geen lucifers meer over zijn. Degene die als laatste een lucifer MOET wegnemen, heeft verloren.')
 
-import random
+while True:
+    print ("Aantal lucifers " , sticks)
+    sticks_taken = int(input("Pak je stokjes(1-4):"))
 
-num1 = random.randint(20, 25)
-y = (num1)
-print (y)
+
