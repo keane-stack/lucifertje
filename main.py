554455302dc351d7e57f82a79fe6ecc80a35a822
keanestack <74552264+keane-stack@users.noreply.github.com>
2021-06-11 06:52:31 +0000

    basisspel staat, alleen de lucifers gaan in de min, dus moet daar nog even een kijkje naar nemen

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ print('')
 print('Het spel gaat als volgt: De computer kiest een willekeurig aantal lucifers tussen de 20 en 25. Dat aantal wordt door de computer als getal aan je getoond. Als speler heb je als eerste de keuze om 1, 2 of 3 lucifers weg te nemen. Daarna is het de beurt aan de computer. Ook de computer moet 1, 2 of 3 lucifers wegnemen, waarna de speler weer aan de beurt is, tot er geen lucifers meer over zijn. Degene die als laatste een lucifer MOET wegnemen, heeft verloren.')
 
 while True:
-    print ("Aantal lucifers " , sticks)
-    sticks_taken = int(input("Pak je stokjes(1-4):"))
-
-
+    print ('Aantal lucifers', sticks)
+    sticks_taken = int(input('Pak je stokjes(1-3):'))
+    if sticks == 1:
+        print('Je hebt het laatste lucifertje gepakt, losertje')
+        break
+    if sticks_taken >= 4 or sticks_taken <= 0:
+        print ('Errorgeval, kies 1, 2 of 3 luficers')
+        continue
+    print ("Computer took: " , (4 - sticks_taken))
+    sticks -= 4
+if sticks == 0:
+  print('Je hebt gewonnen van de computer, whiehoo')
