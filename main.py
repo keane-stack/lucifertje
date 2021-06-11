b501cc36f21857e64f247f9087cdee8af04cdc14
bartrl <74553307+Bart-tabak@users.noreply.github.com>
2021-06-11 16:38:40 +0000

    Random nummer toegevoegd + letters worden niet geaccepteerd

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 #def waardoor als je de game opnieuwstart niet weer de uitleg te zien krijgt
 def introgame():
   global sticks 
@@ -8,16 +10,23 @@ def introgame():
   print('Het spel gaat als volgt: De computer kiest een willekeurig aantal lucifers tussen de 20 en 25. Dat aantal wordt door de computer als getal aan je getoond. Als speler heb je als eerste de keuze om 1, 2 of 3 lucifers weg te nemen. Daarna is het de beurt aan de computer. Ook de computer moet 1, 2 of 3 lucifers wegnemen, waarna de speler weer aan de beurt is, tot er geen lucifers meer over zijn. Degene die als laatste een lucifer MOET wegnemen, heeft verloren.')
 #door deze def kan je het spel restarten
 def game():
-  sticks = 21
+  sticks = random.randrange (20,25)
   while True:
     print ('\nAantal lucifers', sticks)
-    sticks_taken = int(input('Pak je stokjes(1-3):'))
+
+    choice = input('Pak je stokjes(1-3):')
+
+    if not choice.isnumeric():
+      print ("Error, kies 1, 2 of 3")
+      continue
+
+    sticks_taken = int(choice)
     if sticks == 1:
         print('Je hebt het laatste lucifertje gepakt, losertje')
         einde()
         break
     if sticks_taken >= 4 or sticks_taken <= 0:
-        print ('Errorgeval, kies 1, 2 of 3 luficers')
+        print ('Error, kies 1, 2 of 3')
         continue
 
         #dit laat de computer altijd winnen
