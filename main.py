4758f8758b035feec59bf3a7f40257161541e81f
keanestack <74552264+keane-stack@users.noreply.github.com>
2021-06-11 11:54:59 +0000

    alles in een def en gobal sticks toegevoegd

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
-sticks = 21
-print('Naam invullen:')
-x = input()
-print('Gegroet ' + x + ', Welkom bij ons luciferspel!')
-print('')
-print('Het spel gaat als volgt: De computer kiest een willekeurig aantal lucifers tussen de 20 en 25. Dat aantal wordt door de computer als getal aan je getoond. Als speler heb je als eerste de keuze om 1, 2 of 3 lucifers weg te nemen. Daarna is het de beurt aan de computer. Ook de computer moet 1, 2 of 3 lucifers wegnemen, waarna de speler weer aan de beurt is, tot er geen lucifers meer over zijn. Degene die als laatste een lucifer MOET wegnemen, heeft verloren.')
+def game():
+  global sticks 
+  sticks = 21
+  print('Naam invullen:')
+  x = input()
+  print('Gegroet ' + x + ', Welkom bij ons luciferspel!')
+  print('')
+  print('Het spel gaat als volgt: De computer kiest een willekeurig aantal lucifers tussen de 20 en 25. Dat aantal wordt door de computer als getal aan je getoond. Als speler heb je als eerste de keuze om 1, 2 of 3 lucifers weg te nemen. Daarna is het de beurt aan de computer. Ook de computer moet 1, 2 of 3 lucifers wegnemen, waarna de speler weer aan de beurt is, tot er geen lucifers meer over zijn. Degene die als laatste een lucifer MOET wegnemen, heeft verloren.')
 
-while True:
+  while True:
     print ('Aantal lucifers', sticks)
     sticks_taken = int(input('Pak je stokjes(1-3):'))
     if sticks == 1:
@@ -19,6 +21,9 @@ while True:
         #dit laat de computer altijd winnen
     print ("Computer took: " , (4 - sticks_taken))
     sticks -= 4
-if sticks == 0:
-  print('Je hebt gewonnen van de computer, whiehoo')
+    if sticks == 0: 
+         print('Je hebt gewonnen van de computer, whiehoo')
 
+
+
+ 
